0ad6609175d3d3f8ddb3853a330491a32d829291
R* <64038390+raster0x2a@users.noreply.github.com>
2020-08-10 13:26:41 +0900

    Fix command_escape.py

--- commands/command_escape.py
+++ commands/command_escape.py
@@ -1,6 +1,6 @@
 import discord
 
-async def command_escape(message, arg):
+async def main(message, arg):
     
     usr_msg = await message.channel.fetch_message(int(arg.split()[0]))
     new_str = discord.utils.escape_markdown(usr_msg.content)
