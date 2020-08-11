0656014baa18342fa691141bbe98c3ac5cd7276e
Amadeus_vn <git@amas.dev>
2020-08-12 05:25:14 +0900

    support type file

--- bots.py
+++ bots.py
@@ -1,9 +1,10 @@
 import discord
 from subprocess import TimeoutExpired, check_output, CalledProcessError, STDOUT
-from os.path import dirname
+from os.path import dirname, splitext
 from os import listdir
 from proto import cmdin_pb2, cmdout_pb2, help_pb2
 from config import PREFIX
+from io import BytesIO
 
 def count_bquates(st):
     mx = 0
@@ -59,6 +60,7 @@ async def loader(cmd, message, arg):
 
         error = False
         for i, media in enumerate(msg_pb.medias):
+            file = None
             error = error or media.error
             if media.type == media.UTF8:
                 if media.level == 0:
@@ -82,25 +84,37 @@ async def loader(cmd, message, arg):
                 else:
                     f['title'] += media.data.decode(encoding='utf-8')
                     continue
-            if i == len(msg_pb.medias) or not media.extend_field:
+            elif media.type == media.FILE:
+                file = discord.File(BytesIO(media.data), filename=media.filename, spoiler=media.spoiled)
+                _, ext = splitext(media.filename)
+                if ext == ".jpg" or ext == ".png" or ext == ".jpeg":
+                    embed.set_image(url=f'attachment://{media.filename}')
+
+            if i + 1 == len(msg_pb.medias) or not media.extend_field:
                 fields.append(f)
                 f = {'title': '','val': ''}
 
-        embed = discord.Embed()
-        if len(fields) == 1 and fields[0]['title'] == '':
-            embed.description = fields[0]['val']
-        else:
-            for f in fields:
+            if i + 1 == len(msg_pb.medias) or media.type == media.FILE:
+                embed = discord.Embed()
+                if len(fields) == 1 and fields[0]['title'] == '':
+                    embed.description = fields[0]['val']
+                else:
+                    for f in fields:
+                        embed.add_field(name=cmd if f['title'] == '' else f['title'] , value=f['val'])
+                embed.color = msg_pb.color & 0x00ffffff
+                if error:
+                    embed.color = 0xff0000
+                    embed.title = 'Error'
+                embed.set_author(
+                    name=message.author.name,
+                    icon_url=message.author.avatar_url
+                )
+                if file == None:
+                    result.append(await message.channel.send(embed=embed))
+                else:
+                    result.append(await message.channel.send(embed=embed, file=file))
+                    file = None
 
-                embed.add_field(name=cmd if f['title'] == '' else f['title'] , value=f['val'])
-        embed.color = msg_pb.color & 0x00ffffff
-        if error:
-            embed.color = 0xff0000
-            embed.title = 'Error'
-        embed.set_author(
-            name=message.author.name,
-            icon_url=message.author.avatar_url
-        )
-        result.append(await message.channel.send(embed=embed))
+                fields = []
 
     return result
