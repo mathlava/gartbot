3b558075e4e06ec3c51d6df837f46a2995cd7731
Gart <57865205+Gart2357@users.noreply.github.com>
2020-11-08 01:36:51 +0900

    Update command_latex.py

--- commands/command_latex.py
+++ commands/command_latex.py
@@ -13,17 +13,17 @@ async def main(message, arg):
     fid = str(random.SystemRandom().randint(10000, 99999))
     here = os.path.dirname(__file__)
 
-    with open(f'{here}/tex_template/latex.tex', 'r') as f:
+    with open(f'{here}/tex_template/texp.tex', 'r') as f:
         tex_con = f.read().replace('[REPLACE]', arg.strip())
 
-    cp = subprocess.run(["tex2jpg"], input=tex_con.encode('UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cp = subprocess.run(['tex2jpg', '--margins', '10 -10 10 -10'], input=tex_con.encode('UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if cp.returncode != 0:
         embed = discord.Embed(
-            title='レンダリングエラー',
+            title='エラー Error',
             description=f'```\n{cp.stderr.decode("UTF-8")}\n```',
             color=0xff0000
-            )
+        )
         embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         return await message.channel.send(embed=embed)
     else:
