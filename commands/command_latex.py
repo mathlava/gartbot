c6a0178560813a4d9da3ba20d37ff5ac10c45b9a
Gart <57865205+Gart2357@users.noreply.github.com>
2020-11-09 12:23:39 +0900

    Update and rename command_paiza.py to command_latex.py

--- commands/command_latex.py
+++ commands/command_latex.py
@@ -4,12 +4,7 @@ async def main(message, arg):
 
     embed = discord.Embed(
         title='がーとぼっと',
-        description='paizaコマンドはなくなりました\n' \
-            + '代わりに以下のコマンドをお使いください'
-    )
-    embed.add_field(
-        name=')run 言語名\nコード',
-        value='プログラムを実行できます'
+        description='latexコマンドはtexpコマンドに改名しました'
     )
     embed.set_author(
         name=message.author.name,
