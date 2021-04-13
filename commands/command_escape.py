42eeee31ec661d28f79ac33547e869d28b2dbd3c
Gart2357 <gart@outlook.jp>
2021-04-13 15:58:42 +0900

    character count limit

--- commands/command_escape.py
+++ commands/command_escape.py
@@ -26,5 +26,27 @@ async def main(message: discord.Message, arg: str):
             icon_url=message.author.avatar_url
         )
         return await message.reply(embed=embed)
+    if len(usr_msg.content) == 0:
+        embed = discord.Embed(
+            title='エラー',
+            description='文章が含まれるメッセージに返信してください',
+            color=0xff0000
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
     new_str = discord.utils.escape_markdown(usr_msg.content)
+    if len(new_str) > 2000:
+        embed = discord.Embed(
+            title='エラー',
+            description='結果が2000文字を超えました',
+            color=0xff0000
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
     return await message.reply(new_str)
--- commands/command_pekofy.py
+++ commands/command_pekofy.py
@@ -11,13 +11,28 @@ async def main(message: discord.Message, arg: str):
         sentence = referenced_message.content
     else:
         sentence = arg
+    if len(sentence) == 0:
+        embed = discord.Embed(
+            title='エラー',
+            description='文章が含まれるメッセージに返信するか引数に指定してください',
+            color=0xff0000
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
+
     tokens = tokenizer_obj.tokenize(sentence, mode)
     pekofied_sentence = ''
     flag = False
     for t in tokens:
         if flag:
             if t.part_of_speech()[1] == '終助詞':
-                pekofied_sentence += t.surface()
+                if t.dictionary_form() == 'じゃん':
+                    pekofied_sentence += 'ぺこ' + t.surface()
+                else:
+                    pekofied_sentence += t.surface()
             elif t.part_of_speech()[1] == '接続助詞':
                 if t.dictionary_form() == 'と':
                     pekofied_sentence += t.surface()
@@ -33,4 +48,15 @@ async def main(message: discord.Message, arg: str):
             pekofied_sentence += t.surface()
     if flag:
         pekofied_sentence += 'ぺこ'
+    if len(pekofied_sentence) > 2000:
+        embed = discord.Embed(
+            title='エラー',
+            description='結果が2000文字を超えました',
+            color=0xff0000
+        )
+        embed.set_author(
+            name=message.author.name,
+            icon_url=message.author.avatar_url
+        )
+        return await message.reply(embed=embed)
     return await message.reply(pekofied_sentence)
